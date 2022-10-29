076ad6fbbd4b7e7fd771bdb6cd26e4de7651fd65
Okitsune <9houji1889k@gmail.com>
2022-10-30 00:12:03 +0900

    Configure vscode project for unit testing

---
+++ src/AppTest.java
@@ -0,0 +1,18 @@
+   
+    import static org.junit.Assert.assertTrue;
+    import static org.junit.Assert.assertFalse;
+    import org.junit.Test;
+     
+    public class AppTest {
+        @Test
+        public void sampleTestTrue()
+        {
+            assertTrue(true);
+        }
+        
+        @Test
+        public void sampleTestFalse()
+        {
+            assertFalse(false);
+        }
+    }
